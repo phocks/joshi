3273d1b6c3c4a5293c6b0c27b94bad9d557b1595
Joshua Byrd <phocks@gmail.com>
2023-03-12 17:08:12 +1000

    Try a different method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::path::Path;
-use actix_web::{ web, App, HttpResponse, HttpServer, Responder, post, get };
-use std::process::Command;
+use actix_web::{ App, HttpResponse, HttpServer, Responder, post, get };
+use std::process::{Command, Stdio};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -18,7 +18,7 @@ async fn index() -> impl Responder {
 }
 
 #[post("/trigger123")]
-async fn webhook(req_body: String) -> impl Responder {
+async fn webhook(_: String) -> impl Responder {
   // Get the current user's home directory
   let home_dir = match dirs::home_dir() {
     Some(home_dir) => home_dir,
@@ -34,15 +34,20 @@ async fn webhook(req_body: String) -> impl Responder {
   let current_dir = env::current_dir().unwrap();
   let script_path = current_dir.join("get-build-deploy.sh");
 
-  let output = Command::new("sh")
+  let mut cmd = Command::new("sh")
     .arg("-c")
     .arg(script_path)
-    .output()
+    // .output()
+    // .expect("Failed to execute command");
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()
     .expect("Failed to execute command");
 
-  let output_str = String::from_utf8_lossy(&output.stdout).to_string();
+  // let output_str = String::from_utf8_lossy(&output.stdout).to_string();
+  let status = cmd.wait().expect("Failed to wait for command");
 
-  HttpResponse::Ok().body(output_str)
+  HttpResponse::Ok().body("output_str")
 }
 
 #[actix_web::main]
